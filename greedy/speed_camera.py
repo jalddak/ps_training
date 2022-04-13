120d83cdf9c9054d6b3cb144a75a15f952149cb7
jalddak <jalddak98@gmail.com>
2022-04-13 21:36:10 +0900

    greedy_6

---
+++ greedy/speed_camera.py
@@ -0,0 +1,28 @@
+routes = [[0,12],[1,12],[2,12],[3,12],[5,6],[6,12],[10,12]]
+
+def solution(routes):
+    answer = 0
+    routes.sort(key = lambda x: (x[0], x[1]))
+    i = 0
+    while i < len(routes):
+        j = i + 1
+        short = routes[i][1]
+        while j < len(routes):
+            if routes[j][0] <= short:
+                if short > routes[j][1]:
+                    short = routes[j][1]
+                j += 1
+            else:
+                i = j
+                break
+        if j == len(routes):
+            i = j
+        answer += 1
+        if i != j:
+            i += 1
+    
+    print(answer)
+
+    return answer
+
+solution(routes)
