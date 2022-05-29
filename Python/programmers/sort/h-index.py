4afadf4f71f6d6ca8c11d155e50636261010e1ff
HyeonJin Han <97790516+jalddak@users.noreply.github.com>
2022-05-30 01:33:48 +0900

    Delete Python directory

--- Python/programmers/sort/h-index.py
+++
@@ -1,7 +0,0 @@
-def solution(citations):
-    answer = 0
-    citations.sort(reverse = True)
-    for i in range(1, len(citations)+1):
-        if citations[i-1] >= i and len(citations) - i <= i:
-            answer = i
-    return answer
