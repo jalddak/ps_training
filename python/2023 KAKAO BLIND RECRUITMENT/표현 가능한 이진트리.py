4e4b2f038d1326a9cd0108baade2d63064db23fd
jalddak <jalddak98@gmail.com>
2023-06-19 22:27:00 +0900

    설명추가

--- python/2023 KAKAO BLIND RECRUITMENT/표현 가능한 이진트리.py
+++ python/2023 KAKAO BLIND RECRUITMENT/표현 가능한 이진트리.py
@@ -3,13 +3,16 @@ from collections import deque
 def dfs(nums):
     for i in range(len(nums)):
         nextnums = [nums[i][:len(nums[i])//2], nums[i][len(nums[i])//2+1:]]
-    
+
+        # 가운데가 0이라도 자식 노드에 1이 없으면 0이 와도 된다.
         if nums[i][len(nums[i])//2] != '1':
             for sub in nextnums:
                 if '1' in list(sub):
                     return False
+        # 자식노드 포함 길이가 3 이하인데 가운데가 1이면 안봐도 통과다
         elif len(nums[i]) <= 3:
             continue
+        # 자식노드도 확인해본다.
         else:
             result = dfs(nextnums)
             if result == False:
@@ -26,13 +29,14 @@ def solution(numbers):
     for num in binarys:
         n = 0
         check = 0
-        candidates = []
         while 2 ** n - 1 < len(num):
             n += 1
             if 2**n-1 == len(num):
                 check = 1
         
         dif = 2**n-1 - len(num)
+        # 이 부분에서 나는 뒤에도 0을 게속 추가해준것도 생각하다가 게속 틀렸었는데, 뒤에 0을 추가해버리면 아예 숫자가 달라져서 하면안된다.
+        # ex) 4 : 100 = 00100 != 10000 = 16
         if check == 0:
             c = deque(list(num))
             for i in range(dif):
