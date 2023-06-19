7f1fe5339d33f79a3bb601e9adea30ea7b10a861
jalddak <jalddak98@gmail.com>
2023-06-19 22:15:54 +0900

    2023 KAKAO BLIND RECRUITMENT

--- python/2023 KAKAO BLIND RECRUITMENT/표현 가능한 이진트리.py
+++ python/2023 KAKAO BLIND RECRUITMENT/표현 가능한 이진트리.py
@@ -3,12 +3,13 @@ from collections import deque
 def dfs(nums):
     for i in range(len(nums)):
         nextnums = [nums[i][:len(nums[i])//2], nums[i][len(nums[i])//2+1:]]
-        if len(nums[i]) == 1:
-            return True
+    
         if nums[i][len(nums[i])//2] != '1':
             for sub in nextnums:
                 if '1' in list(sub):
                     return False
+        elif len(nums[i]) <= 3:
+            continue
         else:
             result = dfs(nextnums)
             if result == False:
@@ -30,29 +31,19 @@ def solution(numbers):
             n += 1
             if 2**n-1 == len(num):
                 check = 1
-                candidates.append(num)
         
         dif = 2**n-1 - len(num)
         if check == 0:
-            for i in range(dif+1):
-                c = deque(list(num))
-                for _ in range(i):
-                    c.appendleft('0')
-                for _ in range(dif - i):
-                    c.append('0')
-                c = list(c)
-                ctostr = ''
-                for j in range(len(c)):
-                    ctostr += c[j]
-                candidates.append(ctostr)
+            c = deque(list(num))
+            for i in range(dif):
+                c.appendleft('0')
+            num = ''
+            for j in range(len(c)):
+                num += c[j]
         result = 0
-        for i in range(len(candidates)):
-            if candidates[i][len(candidates[i])//2] != '0':
-                result = dfs([candidates[i]])
-            if result:
-                break
+        if num[len(num)//2] != '0':
+            if dfs([num]):
+                result = 1
         answer.append(result)
         
     return answer
-
-print(solution([0]))
