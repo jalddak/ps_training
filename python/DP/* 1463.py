35b742274dcdf06837ae7fc9d964abc38849570a
jalddak <jalddak98@gmail.com>
2023-07-18 07:22:54 +0900

    dp

---
+++ python/DP/* 1463.py
@@ -0,0 +1,17 @@
+def main():
+    n = int(input())
+    dp = [0, 0]
+
+    for i in range(2, n+1):
+        if i % 2 == 0 and i % 3 == 0:
+            dp.append(min(dp[i//2], dp[i//3])+1)
+        elif i % 2 == 0:
+            dp.append(min(dp[i//2], dp[i-1])+1)
+        elif i % 3 == 0:
+            dp.append(min(dp[i//3], dp[i-1])+1)
+        else:
+            dp.append(dp[i-1]+1)
+    print(dp[n])
+    
+if __name__ == '__main__':
+    main()
---
+++ python/DP/2839.py
@@ -0,0 +1,13 @@
+def main():
+    w = int(input())
+    n = w // 5
+    for i in range(n, 0, -1):
+        a = w - 5*i
+
+        if a % 3 == 0:
+            print(i+a//3)
+            return 0
+    print(-1)
+
+if __name__ == '__main__':
+    main()
