4c031b54bd8107bf1d48e625790eb1a44f080458
jalddak <jalddak98@gmail.com>
2023-07-30 23:03:27 +0900

    bfs

--- python/DFS_BFS/2206.py
+++ python/DFS_BFS/2206.py
@@ -9,14 +9,17 @@ dx = [0, 1, 0, -1]
 
 # y, x, cnt, break
 queue = deque([[0, 0, 1, 0]])
-visited = [[False for _ in range(M)] for _ in range(N)]
+visited = [[[0, 0] for _ in range(M)] for _ in range(N)]
 
 while len(queue) != 0:
     y, x, cnt, b = queue.popleft()
+    if y == N-1 and x == M-1 and board[y][x] == 0:
+        print(cnt)
+        exit()
     for d in range(4):
         ny = y + dy[d]
         nx = x + dx[d]
-        if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+        if 0 <= ny < N and 0 <= nx < M and visited[ny][nx][b] == 0:
             if b == 0 or board[ny][nx] == 0:
                 nb = b
                 if ny == N-1 and nx == M-1:
@@ -24,7 +27,7 @@ while len(queue) != 0:
                     exit()
                 if board[ny][nx] == 1:
                     nb += 1
-                visited[ny][nx] = True
+                visited[ny][nx][nb] = 1
                 queue.append([ny, nx, cnt+1, nb])
 
 print(-1)
