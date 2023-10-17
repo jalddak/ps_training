3800742da086c2bc41fbb0ae435e944e1977123a
jalddak <jalddak98@gmail.com>
2023-10-18 01:38:37 +0900

    2023/10/18

--- python/DFS_BFS/* 2458.py
+++ python/DFS_BFS/* 2458.py
@@ -1,6 +1,38 @@
 # DFS
 
-##### 방법 2, 시간초과
+##### 방법 3, pypy3로만 통괴.
+N, M = list(map(int, input().split()))
+
+board = [[-1 for _ in range(N)] for _ in range(N)]
+
+for _ in range(M):
+    s, t = list(map(lambda x: x-1, list(map(int, input().split()))))
+    board[s][t] = 1
+
+for i in range(N):
+    board[i][i] = 1
+
+# 플로이드 워셜 에선 가장 첫번째 포문이 미들값 여기서는 k로 하라고 한다. 왜 인진 이해 아직 못함. 걍 그러라니까 그래야지
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1 or (board[i][k] == 1 and board[k][j] == 1):
+                board[i][j] = 1
+
+result = 0
+for i in range(N):
+    flag = 1
+    for j in range(N):
+        if board[i][j] == 1 or board[j][i] == 1:
+            continue
+        else:
+            flag = 0
+    if flag == 1:
+        result += 1
+
+print(result)
+
+##### 방법 2, 시간초과 // 다시 해보니까 pypy3로 통과함.
 # N, M = list(map(int, input().split()))
 
 # board = [[-1 for _ in range(N)] for _ in range(N)]
@@ -14,9 +46,9 @@
 # for i in range(N):
 #     board[i][i] = 1
 
-# for i in range(N):
-#     for j in range(N):
-#         for k in range(N):
+# for k in range(N):
+#     for i in range(N):
+#         for j in range(N):
 #             if board[i][j] == 1 or (board[i][k] == 1 and board[k][j] == 1):
 #                 board[i][j] = 1
 #             if board2[i][j] == 1 or (board2[i][k] == 1 and board2[k][j] == 1):
