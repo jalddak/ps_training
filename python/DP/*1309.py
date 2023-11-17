d6217f37a3b56615f70f23c07592c568a887ee0f
jalddak <jalddak98@gmail.com>
2023-11-17 15:16:58 +0900

    2023/11/17

--- python/DP/*1309.py
+++ python/DP/*1309.py
@@ -12,3 +12,11 @@ print(sum(dp[0])%9901)
 
 # n = next, b = before
 # next = ne + nl + nr == 2 (e + l + r) + e = 2 (e + l + r) + (be + bl + br) = 2(now) + before
+
+N = int(input())
+dp = [1, 3]
+
+for _ in range(1, N):
+    dp.append((2*dp[-1] + dp[-2]) % 9901)
+
+print(dp[-1])
