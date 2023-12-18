39cdcf8eabca7675f403dcb708ea92dafbaaacc1
jalddak <jalddak98@gmail.com>
2023-12-18 10:08:14 +0900

    2023/12/18: 크루스칼 알고리즘 공부

--- python/solved.ac/class5/! 1197.py
+++ python/solved.ac/class5/! 1197.py
@@ -1,4 +1,5 @@
 # 양방향이면 양쪽에 넣어줘야지
+# 내가 사용한 알고리즘은 프림알고리즘
 
 import sys
 input = sys.stdin.readline
@@ -28,3 +29,37 @@ while len(visited) < V:
         heapq.heappush(heap, (n_cost, n_node))
 
 print(result)
+
+
+# 다른 사람이 작성한 union find 사용해서 크루스칼로 최소스패닝트리 구한 방법
+
+import sys
+input = sys.stdin.readline
+ 
+V, E = map(int, input().split())
+Vroot = [i for i in range(V+1)]
+Elist = []
+for _ in range(E):
+    Elist.append(list(map(int, input().split())))
+ 
+Elist.sort(key=lambda x: x[2])
+ 
+ 
+def find(x):
+    if x != Vroot[x]:
+        Vroot[x] = find(Vroot[x])
+    return Vroot[x]
+ 
+ 
+answer = 0
+for s, e, w in Elist:
+    sRoot = find(s)
+    eRoot = find(e)
+    if sRoot != eRoot:
+        if sRoot > eRoot:
+            Vroot[sRoot] = eRoot
+        else:
+            Vroot[eRoot] = sRoot
+        answer += w
+ 
+print(answer)
