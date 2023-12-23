e548380dfabbbf12ed98bcaf7d289732d2c65be7
jalddak <jalddak98@gmail.com>
2023-12-23 10:16:39 +0900

    2023/12/23

--- python/solved.ac/class5/* 9252.py
+++ python/solved.ac/class5/* 9252.py
@@ -17,3 +17,32 @@ length = len(board[-1][-1])
 print(length)
 if length:
     print(board[-1][-1])
+
+
+first = input()
+second = input()
+
+board = [[0 for _ in range(len(second)+1)] for _ in range(len(first)+1)]
+
+for i in range(1, len(first)+1):
+    for j in range(1, len(second)+1):
+        if first[i-1] == second[j-1]:
+            board[i][j] = board[i-1][j-1] + 1
+        else:
+            board[i][j] = max(board[i-1][j], board[i][j-1])
+
+
+print(board[-1][-1])
+stack = []
+fn = len(first)
+sn = len(second)
+while len(stack) != board[-1][-1]:
+    if board[fn][sn] == board[fn-1][sn]:
+        fn -= 1
+    elif board[fn][sn] == board[fn][sn-1]:
+        sn -= 1
+    else:
+        stack.append(first[fn-1])
+        fn -= 1
+        sn -= 1
+print("".join(reversed(stack)))
