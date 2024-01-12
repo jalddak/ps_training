84c2366314c92bf628f18b77df39b723564f070c
jalddak <jalddak98@gmail.com>
2024-01-13 04:15:29 +0900

    2024/01/13

--- python/이분탐색/2343.py
+++ python/이분탐색/2343.py
@@ -12,14 +12,17 @@ while min_t + 1 < max_t:
 
     temp = 0
     cnt = 1
+    check = True
     for n in nums:
         if temp + n <= mid:
             temp += n
         else:
             temp = n
-            cnt += temp // mid + (1 if temp % mid != 0 else 0)
+            cnt += 1
+            if temp > mid:
+                check = False
 
-    if cnt > M:
+    if cnt > M or not check:
         min_t = mid
     else:
         max_t = mid
