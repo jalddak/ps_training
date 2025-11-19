1f39af7af142e2ab7013c2d19218677313114856
jalddak <jalddak98@gmail.com>
2025-11-19 15:37:41 +0900

    2025/11/19

--- java/src/main/java/ps_traning/baekjoon/all/No_14238.java
+++ java/src/main/java/ps_traning/baekjoon/all/No_14238.java
@@ -1,10 +1,12 @@
+package ps_traning.baekjoon.all;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-public class Main {
+public class No_14238 {
 
     private static int len;
     private static Map<Character, Integer> cnts = new HashMap<>();
