41b5e3380468909346a11988def21e3f555e918c
jalddak <jalddak98@gmail.com>
2025-11-28 10:52:19 +0900

    2025/11/28

--- java/src/main/java/ps_traning/baekjoon/all/No_7511.java
+++ java/src/main/java/ps_traning/baekjoon/all/No_7511.java
@@ -1,8 +1,10 @@
+package ps_traning.baekjoon.all;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class No_7511 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
